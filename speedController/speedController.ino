e654048de5dfb828bc70667883185f6c9f41e381
Russell Buchanan <russellaabuchanan@gmail.com>
2014-11-22 13:45:52 -0500

    add directional control for left and right

--- speedController/speedController.ino
+++ speedController/speedController.ino
@@ -1,23 +1,37 @@
 
-
-int forward = 13;
-int backward = 12;
-int leftMotorPin = 6;
-int rightMotorPin = 7;
+//Pin declarations
+int leftForwardPin = 13;
+int leftBackwardPin = 12;
+int rightForwardPin = 4;
+int rightBackwardPin = 2;
+int leftMotorSpeedPin = 6;
+int rightMotorSpeedPin = 5;
   
 void setup()
 { 
-  pinMode(forward, OUTPUT);
-  pinMode(backward, OUTPUT);
+  //Set digital
+  
+  pinMode(leftForwardPin, OUTPUT);
+  pinMode(leftBackwardPin, OUTPUT);
+  
+  pinMode(rightForwardPin, OUTPUT);
+  pinMode(rightBackwardPin, OUTPUT);
+  
+  pinMode(leftMotorSpeedPin, OUTPUT);
+  pinMode(rightMotorSpeedPin, OUTPUT);
+
 
 }
 
 void loop()
 {
-  digitalWrite(forward, LOW);
-  digitalWrite(backward, HIGH);
+  digitalWrite(leftForwardPin, LOW);
+  digitalWrite(leftBackwardPin, HIGH);
+  
+  digitalWrite(rightForwardPin, LOW);
+  digitalWrite(rightBackwardPin, HIGH);
 
 
-  analogWrite(leftMotorPin, 255);
-  analogWrite(rightMotorPin, 255);
+  analogWrite(leftMotorSpeedPin, 255);
+  analogWrite(rightMotorSpeedPin, 255);
 }
