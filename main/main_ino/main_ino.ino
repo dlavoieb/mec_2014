0159134c314268391633e740776636bf11443d7c
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2014-11-22 16:01:13 -0500

    merged two sensor input

--- main/main_ino/main_ino.ino
+++ main/main_ino/main_ino.ino
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 #define FILTER_WINDOW 50
-#define LIGHT_THRESHOLD 1.80
+#define LIGHT_THRESHOLD 4
 
-int leftForwardPin = 13;
-int leftBackwardPin = 12;
+int leftForwardPin = 11;
+int leftBackwardPin = 10;
 int rightForwardPin = 4;
 int rightBackwardPin = 2;
 int leftMotorSpeedPin = 6;
@@ -20,6 +20,7 @@ int value2;
 
 char tmp1[10];
 char tmp2[10];
+
 LinkedList<int> listS1 = LinkedList<int>();
 LinkedList<int> listS2 = LinkedList<int>();
 
@@ -90,11 +91,11 @@ void loop()
 	for(int i=0; i<listS1.size(); i++){
 	    sum1 += listS1.get(i);
 	}
-	float avg1 = sum1/float(listS1.size());
+	float avg1 = sum1/float(listS1.size()) - 3;
 	for(int i=0; i<listS2.size(); i++){
 	    sum2 += listS2.get(i);
 	}
-	float avg2 = sum2/float(listS2.size());
+	float avg2 = sum2/float(listS2.size()) - 19;
 
   	String stringOne = "Sensor value: ";
   	dtostrf(avg1,1,2,tmp1);
@@ -103,16 +104,17 @@ void loop()
   	String stringThree = stringOne + tmp1 + "\t" + tmp2;
   	
   	Serial.println(stringThree);
-  
-  	if (avg1 > LIGHT_THRESHOLD){
-  		//inside sensor is over line
+
+	float realAvg = (avg2 + avg1)/2.0;
+
+   	if (realAvg > LIGHT_THRESHOLD){
+  		//black
   		turnClockwise();
-  	} else if (avg2 < LIGHT_THRESHOLD){
-  		//outside sensor is not over line
-  		turnCounterClockwise();
   	} else {
-  		moveForward();
+  		//white
+  		turnCounterClockwise();
   	}
+
 	delay(1);
 
 }
